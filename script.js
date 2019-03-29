c6994a2b29aec3e9adc77e158bdbe44e98651634
Renata Cavalheiro da Silva <recavalheiro89@gmail.com>
2019-03-29 16:02:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 // by Mark Zamoyta
 //
 
+//document.write("Hello");
+
 //Card variables
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
     values = ['Ace', 'King', 'Queen', 'Jack',
     'Ten', 'Nine', 'Eight', 'Seven', 'Six',
     'Five', 'Four', 'Three', 'Two'];
+
 
 //DOM variables
 let textArea = document.getElementById('textArea'),
